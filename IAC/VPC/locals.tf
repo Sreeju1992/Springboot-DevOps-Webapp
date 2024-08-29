7f97d2f8790c701afe4a3b385021261951ae6b3d
Sreejith Sreekumar <sreejith.sreekumar1992@gmail.com>
2024-08-29 11:56:10 +0530

    Terraform code to create VPC

---
+++ IAC/VPC/locals.tf
@@ -0,0 +1,10 @@
+locals {
+  name   = "Rakbank-VPC-Dev"
+  region = "us-east-1"
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 2)
+  common_tags = {
+    owners = "Rakbank"
+    environment = "dev"
+  }
+}
---
+++ IAC/VPC/main.tf
@@ -0,0 +1,36 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
+  name = local.name
+  cidr = local.vpc_cidr
+  azs = local.azs
+
+  private_subnets     =  [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  public_subnets      =  [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
+
+  map_public_ip_on_launch = true
+
+
+# NAT gateways - Outbound communication
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+# VPC DNS parameters
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  public_subnet_tags = {
+    Type = "public-subnets"
+  }
+
+  private_subnet_tags = {
+    Type = "private-subnets"
+  }
+
+ 
+  tags = local.common_tags
+
+  vpc_tags = {
+    Name = "vpc-dev"
+  }
+}
