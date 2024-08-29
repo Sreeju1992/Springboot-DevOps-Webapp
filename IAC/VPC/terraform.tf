f8adf9896e8bbf436652b5d9123538f9b3b9d9bc
Sreejith Sreekumar <sreejith.sreekumar1992@gmail.com>
2024-08-29 13:26:32 +0530

    Modified the bucket name

--- IAC/VPC/terraform.tf
+++ IAC/VPC/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket1992"   # Replace with your S3 bucket name
+    bucket         = "terraform-state-bucket-rakbank"   # Replace with your S3 bucket name
     key            = "VPCstatefile" # Path within the bucket where the state file will be stored
     region         = "us-east-1"                    # Replace with your desired AWS region
     encrypt        = true                           # Encrypt the state file at rest
