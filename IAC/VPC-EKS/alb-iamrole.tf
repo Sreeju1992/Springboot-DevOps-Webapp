5441699e28e556c88dd388ddec00202acc78d5d8
Sreejith Sreekumar <sreejith.sreekumar1992@gmail.com>
2024-08-31 23:51:54 +0530

    ALB changes committed again

---
+++ IAC/VPC-EKS/alb-iamrole.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_role" "alb_ingress_controller_role" {
+  name = "ALBIngressControllerRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${replace(aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer, "https://", "")}"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:alb-ingress-controller"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "alb_ingress_controller_policy_attachment" {
+  role       = aws_iam_role.alb_ingress_controller_role.name
+  policy_arn  = aws_iam_policy.alb_ingress_controller_policy.arn
+}
---
+++ IAC/VPC-EKS/aws-load-balancer-controller.tf
@@ -0,0 +1,49 @@
+resource "helm_release" "aws_load_balancer_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = "1.7.2"
+
+  namespace = "kube-system"
+
+  set {
+    name  = "clusterName"
+    value = aws_eks_cluster.eks_cluster.name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "awsRegion"
+    value = var.region
+  }
+
+  set {
+    name  = "vpcId"
+    value = module.vpc.vpc_id
+  }
+
+  set {
+    name  = "image.repository"
+    value = "602401143452.dkr.ecr.us-east-1.amazonaws.com/amazon/aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "image.tag"
+    value = "v2.7.2"  # Ensure the image tag matches the version of the controller you are using
+  }
+
+  set {
+    name  = "aws.loadBalancerController.enable"
+    value = "true"  # Ensure the controller is enabled
+  }
+
+}
