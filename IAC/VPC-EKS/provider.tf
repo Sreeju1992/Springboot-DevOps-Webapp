da4ed1e9bc09554440358448cb06847fa766e17c
Sreejith Sreekumar <sreejith.sreekumar1992@gmail.com>
2024-09-01 10:55:38 +0530

    Updated provider.tf file

--- IAC/VPC-EKS/provider.tf
+++ IAC/VPC-EKS/provider.tf
@@ -2,12 +2,6 @@ provider "aws" {
     region = local.region
 }
 
-provider "kubernetes" {
-  host                   = aws_eks_cluster.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks_cluster.token
-}
-
 provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.eks_cluster.endpoint
@@ -15,6 +9,7 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.eks_cluster.token
   }
 }
+
 
 # Terraform block
 terraform {
